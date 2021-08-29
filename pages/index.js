14cfc9bdc0b5729b311a2110498d89a11a28831b
YJ Soon <yjsoon@tinkertanker.com>
2021-08-29 20:48:34 +0800

    Set up pagination

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,37 @@
 import Head from "next/head";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [page, setPage] = useState(1);
+
+  const nextPage = async (event) => {
+    event.preventDefault();
+    setPage(page + 1);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Will Generator</title>
       </Head>
-
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">Will Generator</h1>
+        <form onSubmit={nextPage}>
+          {page == 1 && (
+            <>
+              <label htmlFor={"name"}>Name</label>
+              <input type="text" id={"name"} />
+            </>
+          )}
+
+          {page == 2 && (
+            <>
+              <label htmlFor={"name"}>Page 2 form</label>
+              <input type="text" id={"name"} />
+            </>
+          )}
+          <button>Submit</button>
+        </form>
       </main>
     </div>
   );
