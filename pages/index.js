8688b46791da533360e1549ed7f9e014c7f78be6
YJ Soon <yjsoon@tinkertanker.com>
2021-08-29 21:16:39 +0800

    Set up Tailwind globals file

--- pages/index.js
+++ pages/index.js
@@ -16,22 +16,27 @@ export default function Home() {
       </Head>
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">Will Generator</h1>
-        <form onSubmit={nextPage}>
-          {page == 1 && (
-            <>
-              <label htmlFor={"name"}>Name</label>
-              <input type="text" id={"name"} />
-            </>
-          )}
+        <section className="p-10 bg-gray-100 m-4">
+          <form onSubmit={nextPage}>
+            {page == 1 && (
+              <>
+                <label htmlFor={"name"}>Name</label>
+                <input type="text" id={"name"} />
+              </>
+            )}
 
-          {page == 2 && (
-            <>
-              <label htmlFor={"name"}>Page 2 form</label>
-              <input type="text" id={"name"} />
-            </>
-          )}
-          <button>Submit</button>
-        </form>
+            {page == 2 && (
+              <>
+                <label htmlFor={"name"}>Page 2 form</label>
+                <input type="text" id={"name"} />
+              </>
+            )}
+            <br />
+            <button className="p-4 rounded bg-blue-100 border-red-400 mt-4">
+              Submit
+            </button>
+          </form>
+        </section>
       </main>
     </div>
   );
