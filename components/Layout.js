f98f4fc70fe77bceb684e234ac0164113e56e3eb
YJ Soon <yjsoon@tinkertanker.com>
2021-09-01 22:41:42 +0800

    Updated with footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,15 @@
 const Layout = ({ children }) => {
   return (
-    <div className="antialiased text-gray-900 bg-gray-400 py-10 h-full">
-      <main className="max-w-4xl mx-auto py-12 md:mx-w-4xl bg-white px-10">
-        <h1 className="text-6xl font-bold">Will Generator</h1>
-        {children}
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="antialiased text-gray-900 py-10 align-center flex flex-col flex-1 items-center">
+        <main className="max-w-4xl py-12 md:mx-w-4xl bg-white px-10">
+          <h1 className="text-6xl font-bold">Will Generator</h1>
+          {children}
+        </main>
       </main>
+      <footer className="bg-blue-50 flex items-center justify-center w-full h-24 border-t">
+        &copy; {new Date().getFullYear()} Will G.
+      </footer>
     </div>
   );
 };
--- components/PersonAdder.js
+++ components/PersonAdder.js
@@ -8,7 +8,7 @@ import { countries } from "../constants/countries";
 
 const PersonAdder = ({ person, setPerson, description }) => {
   return (
-    <div className="divide-y divide-gray-300 bg-gray-100 px-4 py-8 my-4">
+    <div className="rounded divide-y divide-gray-300 bg-gray-100 px-8 py-8 my-4">
       <section className="grid grid-cols-1 md:grid-cols-2 md:gap-x-8 items-start mb-4">
         <PersonInputText
           description={description}
