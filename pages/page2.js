1aa4464f6886441202e49dd8d7cd66070480cc87
YJ Soon <yjsoon@tinkertanker.com>
2021-09-01 22:28:45 +0800

    Added alternative executor button and code

--- pages/page2.js
+++ pages/page2.js
@@ -5,13 +5,14 @@ import { useAppContext } from "../contexts/AppContext";
 
 import PersonAdder from "../components/PersonAdder";
 import NavButtons from "../components/NavButtons";
+import { personTemplate } from "../constants/personTemplate";
 
 const Page2 = () => {
   const { executors, setExecutors } = useAppContext();
-  let executor = executors[0];
-  let setExecutor = (executor) => {
-    setExecutors([executor]);
-  };
+  let mainExecutor = executors[0];
+  let setMainExecutor = (executor) => setExecutors([executor, altExecutor]);
+  let altExecutor = executors[1];
+  let setAltExecutor = (executor) => setExecutors([mainExecutor, executor]);
 
   useEffect(() => {
     console.log(executors);
@@ -23,10 +24,33 @@ const Page2 = () => {
         <title>Will Generator - Page 2</title>
       </Head>
       <PersonAdder
-        person={executor}
-        setPerson={setExecutor}
+        person={mainExecutor}
+        setPerson={setMainExecutor}
         description="Full Name of Main Executor"
       />
+      {executors[1] == null && (
+        <button
+          className="p-4 rounded bg-green-100"
+          onClick={() => setAltExecutor({ ...personTemplate })}
+        >
+          + Add alternative executor
+        </button>
+      )}
+      {executors[1] && (
+        <>
+          <PersonAdder
+            person={altExecutor}
+            setPerson={setAltExecutor}
+            description="Full Name of Main Executor"
+          />
+          <button
+            className="p-4 rounded bg-red-100"
+            onClick={() => setAltExecutor(null)}
+          >
+            Remove alternative executor
+          </button>
+        </>
+      )}
       <NavButtons prev="/" next="page3" />
     </>
   );
