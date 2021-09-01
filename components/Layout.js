1d1097655831b0d42effbc5a19ce51a565fe5ac7
YJ Soon <yjsoon@tinkertanker.com>
2021-09-01 22:47:05 +0800

    Minor CSS fix

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <main className="antialiased text-gray-900 py-10 align-center flex flex-col flex-1 items-center">
+    <div className="antialiased flex flex-col items-center justify-center min-h-screen py-2">
+      <main className=" text-gray-900 py-10 align-center flex flex-col flex-1 items-center">
         <main className="max-w-4xl py-12 md:mx-w-4xl bg-white px-10">
           <h1 className="text-6xl font-bold">Will Generator</h1>
           {children}
