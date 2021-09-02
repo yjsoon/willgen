010fc59a06c8b748cc43067c380cc1d9e456ea41
YJ Soon <yjsoon@tinkertanker.com>
2021-09-02 22:31:12 +0800

    Fixes for flex-grow in layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,16 @@
 const Layout = ({ children }) => {
   return (
-    <div className="antialiased flex flex-col items-center justify-center min-h-screen py-2">
-      <main className=" text-gray-900 py-10 align-center flex flex-col flex-1 items-center">
-        <main className="max-w-4xl py-12 md:mx-w-4xl bg-white px-10">
-          <h1 className="text-6xl font-bold">Will Generator</h1>
+    <>
+      <div className="antialiased flex flex-col items-center justify-center h-screen py-2">
+        <main className="max-w-4xl py-12 md:mx-w-4xl bg-white px-10 flex-grow">
+          <h1 className="text-6xl font-bold mb-10">Will Generator</h1>
           {children}
         </main>
-      </main>
-      <footer className="bg-blue-50 flex items-center justify-center w-full h-24 border-t">
-        &copy; {new Date().getFullYear()} Will G.
-      </footer>
-    </div>
+        <footer className="bg-blue-50 flex items-center justify-center w-full h-24 border-t">
+          &copy; {new Date().getFullYear()} Will G.
+        </footer>
+      </div>
+    </>
   );
 };
 
