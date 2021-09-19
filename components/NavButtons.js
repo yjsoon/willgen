bc4f97ff808301571fceb18bd61773797c54b7cc
YJ Soon <yjsoon@tinkertanker.com>
2021-09-19 15:08:10 +0800

    Added blurb to first page

--- components/NavButtons.js
+++ components/NavButtons.js
@@ -1,18 +1,23 @@
 import Link from "next/link";
 
-const NavButtons = ({ prev, next }) => {
+const NavButtons = ({
+  prev,
+  prevLabel = "Previous",
+  next,
+  nextLabel = "Next",
+}) => {
   return (
     <div className="flex justify-end mt-8">
       {prev && (
         <Link href={prev}>
           <button className="p-4 rounded bg-blue-100 w-40 mr-4">
-            Previous
+            {prevLabel}
           </button>
         </Link>
       )}
       {next && (
         <Link href={next}>
-          <button className="p-4 rounded bg-blue-200 w-40">Next</button>
+          <button className="p-4 rounded bg-blue-200 w-40">{nextLabel}</button>
         </Link>
       )}
     </div>
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,57 @@ export default function Home() {
       <Head>
         <title>Will Generator</title>
       </Head>
-      <NavButtons next="1_about" />
+      <p>
+        Take care of your loved ones by planning your legacy today. After you
+        have decided on your executor(s) and beneficiaries, as well as how much
+        each should receive, use our Online Will Generator to set a plan in
+        place.
+      </p>
+      <h2>Things to note before you begin.</h2>
+      <div className="info grid-cols-3 grid gap-4">
+        <section>
+          <h3>1. You will need the following information</h3>
+          <h4>Mandatory</h4>
+          <ul>
+            <li>Your personal details</li>
+            <li>Name and NRIC/Passport no. of Executors</li>
+            <li>Name and NRIC/Passport no. of Beneficiaries</li>
+          </ul>
+          <h4>Recommended</h4>
+          <ul>
+            <li>
+              Asset details (e.g. list of bank accounts or insurance policies)
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h3>2. Start only when you can finish</h3>
+          <p>
+            This session should take less than 10 minutes to complete if all the
+            information is available.
+          </p>
+          <p>
+            Your response will be saved in this browser, but can be lost if you
+            close the window, or if you clear your cache. Please make sure you
+            have everything you need to complete the process.
+          </p>
+        </section>
+        <section>
+          <h3>The Will is only valid once it has been endorsed</h3>
+          <p>
+            This online template produces a pdf that you need to print and sign.
+            It will only be valid once it has been signed by you and two
+            witnesses.
+          </p>
+          <p>
+            The printed and signed Will should be stored in a safe place (Such
+            as a safe deposit box), with its location added to the{" "}
+            <a href="https://wills.sal.sg/">Wills Registry</a>, and made known
+            to the executor(s).
+          </p>
+        </section>
+      </div>
+      <NavButtons next="1_about" nextLabel="Get started" />
     </>
   );
 }
